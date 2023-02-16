2eadca06401e6fd90e19b4af65d38e0e20912e71
Ronasian <119774818+Ronasian@users.noreply.github.com>
2023-02-15 22:26:00 -0800

    completed css style of webpage

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+var userInput = document.querySelector('input');
+var searchBtn = document.querySelector('#searchBtn');
+var history = document.querySelector('#history');
+var currentWeather = document.querySelector('#current-weather');
+var forecast = document.querySelector('#forecast');
+var firstDay = document.querySelector('day-1');
+var secondDay = document.querySelector('day-2');
+var thirdDay = document.querySelector('day-3');
+var fourthDay = document.querySelector('day-4');
+var fifthDay = document.querySelector('day-5');
+
+function renderWeather() {
+
+}
+
